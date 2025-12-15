8f3f3684de6f0da72b747abd81963eab8b6d02b0
giragon6 <emmemcdonald66@gmail.com>
2025-12-15 10:49:34 -0500

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/careerquest/flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.verygoodcore.careerquest_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:careerquest_flutter/app/app.dart';
+import 'package:careerquest_flutter/bootstrap.dart';
+
+Future<void> main() async {
+  await bootstrap(() => const App());
+}
